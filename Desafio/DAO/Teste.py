c5182d5926ae5019339d10ed96cdb7622f1850da
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2024-08-28 16:00:12 -0300

    Testada insercao de nova classe, aprovado.

--- Desafio/DAO/Teste.py
+++ Desafio/DAO/Teste.py
@@ -6,4 +6,13 @@ def testeLer():
     classes=daoClasse.readAll()
     for c in classes:
         print(str(c.COD)+" classe: "+c.CLASSE)
+
+def testeNovo():
+    novaClasse=daoClasse.classe()
+
+    novaClasse.CLASSE='Scooter'
+    daoClasse.create(novaClasse)
+testeLer()
+testeNovo()
+print("----------------------------------")
 testeLer()
