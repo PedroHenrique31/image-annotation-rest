ad5ef2d8dbf37a66ce432979b0182fadd50d8f3d
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2024-08-28 16:12:06 -0300

    Testada todas as funcoes de daoClasses.

--- Desafio/DAO/Teste.py
+++ Desafio/DAO/Teste.py
@@ -12,7 +12,22 @@ def testeNovo():
 
     novaClasse.CLASSE='Scooter'
     daoClasse.create(novaClasse)
+def testeReadByID():
+    classe=daoClasse.readByID(6)
+    print("Lendo um único registro")
+    print("COD: "+str(classe.COD)+" NOME: "+classe.CLASSE)
+def testeAlterar():
+    print("Alterando o registro")
+    classe=daoClasse.readByID(8)
+    classe.CLASSE='Carroça' # Palavra a ser alterada
+    daoClasse.update()
+def testeExcluir():
+    print("Teste função excluir")
+    classe=daoClasse.readByID(8)
+    daoClasse.delete(classe)
 testeLer()
-testeNovo()
-print("----------------------------------")
+#testeNovo()
+#testeReadByID()
+#testeAlterar()
+#testeExcluir()
 testeLer()
